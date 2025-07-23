f1e633e749cdd78ce81f6f1b5486ca53fc77249f
Ian Allison <iana@pims.math.ca>
2025-07-23 10:12:45 -0700

    Package renamed for ocatve
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- pims-octave/octave-packages.m
+++ pims-octave/octave-packages.m
@@ -4,6 +4,6 @@ pkg install -forge -global ltfat;
 pkg install -forge -global netcdf;
 pkg install -forge -global pkg-octave-doc;
 pkg install -forge -global pythonic;
-pkg install -forge -global statistics-bootstrap;
+pkg install -forge -global statistics-resampling;
 pkg install -forge -global statistics;
 pkg install -forge -global symbolic;
